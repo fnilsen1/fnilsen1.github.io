1074e0812a19ea5c4305a791eaba415312e6bfe0
fnilsen1 <37710022+fnilsen1@users.noreply.github.com>
2022-04-02 01:34:20 +0200

    Add files via upload

---
+++ Kilder/kilder.js
@@ -0,0 +1,13 @@
+function kopier() {
+  let a = document.getElementById("inp1").value;
+  let b = document.getElementById("inp2").value;
+  let c = document.getElementById("inp3").value;
+  let d = document.getElementById("inp4").value;
+  let e = document.getElementById("inp5").value;
+  let f = document.getElementById("inp6").value;
+  let kilde =
+    a + " (" + b + ") " + c + ". Hentet " + d + " fra " + e + ": " + "\n" + f;
+  navigator.clipboard.writeText(kilde);
+  alert("Du kopierte " + "\n" + kilde);
+  document.getElementById("resultat").innerHTML = kilde;
+}
